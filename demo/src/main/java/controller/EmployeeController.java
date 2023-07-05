f753567f54aa1cfd864c043d61536c4fd5cbcc1b
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-07-05 13:23:28 +0300

    adding get employee in service controller model

--- demo/src/main/java/controller/EmployeeController.java
+++ demo/src/main/java/controller/EmployeeController.java
@@ -1,5 +1,26 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Employee;
+import service.IEmployeeService;
+
+@RestController
+@RequestMapping("/api")
 public class EmployeeController {
+	
+	@Autowired
+	private IEmployeeService employeeService;
+	
+	@GetMapping("/employee")
+	public List<Employee> get(){
+		return employeeService.get();
+	}
+	
 
 }
